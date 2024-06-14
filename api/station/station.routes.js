7862fa784c5759cb8cb8acaf487b0daecfd6c75b
Ordoga <ordogga@gmail.com>
2024-06-14 20:16:36 +0300

    Added middleware

--- api/station/station.routes.js
+++ api/station/station.routes.js
@@ -1,13 +1,14 @@
 import express from 'express'
 
 import { getStations, getStation, addStation, updateStation, removeStation } from './station.controller.js'
+import { requireUser } from '../../middlewares/requireAuth.middleware.js'
 
 const router = express.Router()
 
 router.get('/', getStations)
 router.get('/:stationId', getStation)
-router.post('/', addStation)
-router.put('/:stationId', updateStation)
-router.delete('/:stationId', removeStation)
+router.post('/', requireUser, addStation)
+router.put('/:stationId',requireUser, updateStation)
+router.delete('/:stationId', requireUser, removeStation)
 
 export const stationRoutes = router
