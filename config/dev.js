68c2a7b4854bc4704063fcb0edf238b20140603e
Or Doga <ordogga@gmail.com>
2025-01-07 13:47:14 +0200

    New settings

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,4 @@
 export default {
-    dbURL: process.env.MONGO_URL || 'mongodb+srv://ordogga:YXkHZ9v9BFAzc3IH@spotify-web.jajvibh.mongodb.net/',
-    dbName : process.env.DB_NAME || 'SPOTIFY_WEB_DB'
-  }
-  
+    dbURL: process.env.MONGO_URL || 'mongodb+srv://ordogga:ordoga30@spotify-web.jajvibh.mongodb.net/',
+    dbName: process.env.DB_NAME || 'SPOTIFY_WEB_DB',
+}
